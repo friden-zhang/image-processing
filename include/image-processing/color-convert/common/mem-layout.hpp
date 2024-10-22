705f5c213a5ed3aba906b9196002987b2735512c
friden <Zplearner@163.com>
2024-10-22 21:14:32 +0800

    feat(rgb2gray): add rgb2gray packed/planar

---
+++ include/image-processing/color-convert/common/mem-layout.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace image_processing {
+namespace color_convert {
+
+enum class MemLayout { Packed, Planar };
+
+}
+} // namespace image_processing
--- src/color-convert/kernels/cpu/rgb2gray.hpp
+++ src/color-convert/kernels/cpu/rgb2gray.hpp
@@ -6,14 +6,23 @@ namespace color_convert {
 
 namespace kernels {
 
-bool rgb_2_gray_native(const unsigned char *input, unsigned char *output,
-                       int width, int height);
+bool rgb_packed_2_gray_native(const unsigned char *input, unsigned char *output,
+                              int width, int height);
 
-bool rgb_2_gray_parallel(const unsigned char *input, unsigned char *output,
-                         int width, int height);
+bool rgb_packed_2_gray_parallel(const unsigned char *input,
+                                unsigned char *output, int width, int height);
 
-bool rgb_2_gray_simd(const unsigned char *input, unsigned char *output,
-                     int width, int height);
+bool rgb_packed_2_gray_simd(const unsigned char *input, unsigned char *output,
+                            int width, int height);
+
+bool rgb_planar_2_gray_native(const unsigned char *input, unsigned char *output,
+                              int width, int height);
+
+bool rgb_planar_2_gray_parallel(const unsigned char *input,
+                                unsigned char *output, int width, int height);
+
+bool rgb_planar_2_gray_simd(const unsigned char *input, unsigned char *output,
+                            int width, int height);
 } // namespace kernels
 
 } // namespace color_convert
